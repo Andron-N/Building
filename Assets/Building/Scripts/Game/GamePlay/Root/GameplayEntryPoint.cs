f1213730591fc9d4c5a1b8ddd72fdce852d3086f
Andron-N <andron-nwork@gmail.com>
2025-01-12 21:28:28 +0400

    Entry Point: Base Entry Point

---
+++ Assets/Building/Scripts/Game/GamePlay/Root/GameplayEntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.GamePlay.Root
+{
+  public class GameplayEntryPoint : MonoBehaviour
+  {
+    [SerializeField] private GameObject _sceneRootBinder;
+
+    public void Run() =>
+      Debug.Log("Gameplay scene loaded");
+  }
+}
